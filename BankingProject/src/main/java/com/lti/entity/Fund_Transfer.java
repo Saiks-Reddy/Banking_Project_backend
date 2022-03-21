03eed8e20c3ecb41b3fe3f624b4afd0ea4d00169
hrishikesh-ghatol <hrishighatol1956@gmail.com>
2022-03-21 16:54:03 +0530

    Create Fund_Transfer.java
    transfer

---
+++ BankingProject/src/main/java/com/lti/entity/Fund_Transfer.java
@@ -0,0 +1,15 @@
+package com.lti.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_fundtransfer")
+public class Fund_Transfer {
+	
+	
+	
+	
+	
+
+}
