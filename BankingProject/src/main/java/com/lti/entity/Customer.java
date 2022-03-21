0c6cd45278ff8e749059a836a6699508785da9db
hrishikesh-ghatol <hrishighatol1956@gmail.com>
2022-03-21 16:47:53 +0530

    Update Customer.java
    update customer

--- BankingProject/src/main/java/com/lti/entity/Customer.java
+++ BankingProject/src/main/java/com/lti/entity/Customer.java
@@ -1,5 +1,111 @@
 package com.lti.entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_customer")
 public class Customer {
 
+	private String title;
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	private String mobileNumber;
+	private String email;
+	private String aadharNumber;
+	private String dob;
+	private String address;
+	private String accountNumber;
+	@Id
+	private String userName;
+	private String password;
+	private String accountBalance;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAadharNumber() {
+		return aadharNumber;
+	}
+	public void setAadharNumber(String aadharNumber) {
+		this.aadharNumber = aadharNumber;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(String accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	
+	
+	
+	
+	
 }
